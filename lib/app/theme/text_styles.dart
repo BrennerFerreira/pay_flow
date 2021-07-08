c840db679b66fb7cdb8679f7ab0e0885c626ccdb
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-08 11:52:22 -0300

    feat: basic app structure setup

---
+++ lib/app/theme/text_styles.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'colors.dart';
+
+class AppTextStyles {
+  static final titleLogin = GoogleFonts.lexendDeca(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AppColors.title,
+  );
+  static final titleOnPrimary = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.background,
+  );
+  static final titleBold = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.title,
+  );
+  static final titleBoldOnPrimary = GoogleFonts.lexendDeca(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.background,
+  );
+  static final titleListTile = GoogleFonts.lexendDeca(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    color: AppColors.title,
+  );
+  static final trailingRegular = GoogleFonts.lexendDeca(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.title,
+  );
+  static final trailingBold = GoogleFonts.lexendDeca(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.title,
+  );
+  static final buttonPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primary,
+  );
+  static final buttonSecondary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.title,
+  );
+  static final buttonOnPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.background,
+  );
+  static final buttonBoldPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primary,
+  );
+  static final buttonBoldSecondary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.title,
+  );
+  static final buttonBoldOnPrimary = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.background,
+  );
+  static final captionOnPrimary = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.background,
+  );
+  static final captionBody = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.body,
+  );
+  static final captionBoldOnPrimary = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColors.background,
+  );
+  static final captionBoldBody = GoogleFonts.lexendDeca(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColors.body,
+  );
+  static final input = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.input,
+  );
+}
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,76 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+import 'app/app_widget.dart';
 import 'flavor.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MultiProvider(
-        providers: [
-          Provider<AppFlavor>.value(value: AppFlavor.DEV),
-        ],
-        child: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              Provider.of<AppFlavor>(context).toString(),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(AppWidget(AppFlavor.DEV));
 }
