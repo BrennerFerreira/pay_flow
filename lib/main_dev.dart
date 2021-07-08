2477cdd84386dde5dab922e12215694f4d95c1c9
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-08 15:53:43 -0300

    chore: config injectable

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,8 +1,10 @@
+import 'package:boleto_organizer/injectable.dart';
 import 'package:flutter/material.dart';
 
 import 'app/firebase_app.dart';
 import 'flavor.dart';
 
 void main() {
+  configureDependencies();
   runApp(const AppFirebase(AppFlavor.DEV));
 }
