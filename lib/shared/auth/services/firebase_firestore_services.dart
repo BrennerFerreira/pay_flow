853b7594753ef3cb801a86a2da3e576ac4921bea
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-13 12:04:52 -0300

    feat: save user device token to send notifications

--- lib/shared/auth/services/firebase_firestore_services.dart
+++ lib/shared/auth/services/firebase_firestore_services.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injectable/injectable.dart';
 
@@ -26,6 +28,67 @@ class FirebaseFirestoreServices {
       return user;
     } catch (e) {
       return null;
+    }
+  }
+
+  Future<void> saveToken({
+    required String userId,
+    required String token,
+  }) async {
+    try {
+      final tokenData = {
+        TOKEN_TITLE_STRING: token,
+        UPDATED_AT_STRING: DateTime.now().toIso8601String(),
+        PLATFORM_STRING: Platform.operatingSystem,
+      };
+
+      final userDoc = _firestore.collection(USERS_STRING).doc(userId);
+
+      final userDocData = await userDoc.get();
+
+      if (userDocData.data()!.containsKey(TOKENS_STRING)) {
+        final tokens = List.castFrom<dynamic, Map<String, dynamic>>(
+          userDocData.data()![TOKENS_STRING] as List<dynamic>,
+        );
+
+        final sameTokenExists = tokens
+            .where(
+              (element) => element[TOKEN_TITLE_STRING] as String == token,
+            )
+            .toList();
+
+        if (sameTokenExists.isEmpty) {
+          userDoc.set(
+            {
+              TOKENS_STRING: FieldValue.arrayUnion([tokenData])
+            },
+            SetOptions(merge: true),
+          );
+        } else {
+          userDoc.set(
+            {
+              TOKENS_STRING: FieldValue.arrayRemove(sameTokenExists),
+            },
+            SetOptions(merge: true),
+          );
+
+          userDoc.set(
+            {
+              TOKENS_STRING: FieldValue.arrayUnion([tokenData]),
+            },
+            SetOptions(merge: true),
+          );
+        }
+      } else {
+        userDoc.set(
+          {
+            TOKENS_STRING: [tokenData],
+          },
+          SetOptions(merge: true),
+        );
+      }
+    } catch (e) {
+      return;
     }
   }
 
