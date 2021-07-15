4108ce7a67ecd980f2b571319e2cea3e0cb30278
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-15 06:57:31 -0300

    chore: prepare for release

--- lib/flavor.dart
+++ lib/flavor.dart
@@ -1,2 +1,2 @@
 // ignore_for_file: constant_identifier_names
-enum AppFlavor { DEV, PROD }
+enum AppFlavor { DEV, PROD, PREVIEW }
---
+++ lib/main_prev.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'app/firebase_app.dart';
+import 'flavor.dart';
+import 'injectable.dart';
+
+void main() {
+  configureDependencies();
+  runApp(const AppFirebase(AppFlavor.PREVIEW));
+}
