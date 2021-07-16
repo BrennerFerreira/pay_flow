db324c6c96677de09996fd41ff1470c1fa881ca9
Brenner Ferreira <72360351+BrennerFerreira@users.noreply.github.com>
2021-07-16 08:28:08 -0300

    Revert "refactor: extract toast widget"
    This reverts commit e7de3b99d163e572dac0d832a2396980099e1319.

--- lib/modules/insert_boleto/widgets/error_toast/error_toast.dart
+++ lib/modules/insert_boleto/widgets/error_toast/error_toast.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
 
-class CustomToast extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final Color color;
+import '../../../../app/theme/colors.dart';
 
-  const CustomToast({
-    Key? key,
-    required this.icon,
-    required this.label,
-    required this.color,
-  }) : super(key: key);
-
+class ErrorToast extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,16 +10,16 @@ class CustomToast extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 16),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(25.0),
-        color: color,
+        color: AppColors.delete,
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(icon),
-          const SizedBox(width: 12.0),
+        children: const [
+          Icon(Icons.error),
+          SizedBox(width: 12.0),
           Expanded(
             child: Text(
-              label,
+              "Erro ao salvar o boleto. Por favor, tente novamente.",
               softWrap: true,
             ),
           )
