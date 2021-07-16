0e3efc98396bf42bf5f170b946d3bbc49d0c5aab
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-16 08:15:44 -0300

    refactor: replace auth controller listeners

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:boleto_organizer/shared/widgets/toast/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:provider/provider.dart';
