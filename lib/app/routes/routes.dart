43b436b03b01522500dcb765f4e79d26e654ea39
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-16 19:44:31 -0300

    chore: fix imports

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,4 +1,3 @@
-import 'package:boleto_organizer/modules/privacy_policy/privacy_policy_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -7,6 +6,7 @@ import '../../modules/home/home_page.dart';
 import '../../modules/insert_boleto/controller/insert_boleto_controller.dart';
 import '../../modules/insert_boleto/insert_boleto_page.dart';
 import '../../modules/login/login_page.dart';
+import '../../modules/privacy_policy/privacy_policy_page.dart';
 import '../../modules/splash/splash_page.dart';
 import 'routes_names.dart';
 
