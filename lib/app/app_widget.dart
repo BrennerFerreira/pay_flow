21aca7caf11a5d415169d126bf61b5907a9bb044
BrennerFerreira <brennercsferreira@gmail.com>
2021-07-17 11:07:01 -0300

    feat: basic analytics implementation

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:boleto_organizer/shared/analytics/controller/analytics_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
@@ -11,17 +12,21 @@ import 'theme/theme.dart';
 class AppWidget extends StatelessWidget {
   final AppFlavor flavor;
 
-  const AppWidget(this.flavor, {Key? key}) : super(key: key);
+  AppWidget(this.flavor, {Key? key}) : super(key: key);
+
+  final _analyticsController = getIt<AnalyticsController>();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider<AppFlavor>.value(value: flavor),
+        Provider<AnalyticsController>.value(value: _analyticsController),
         ChangeNotifierProvider<AuthController>(
           create: (_) => getIt<AuthController>(),
         ),
       ],
-      builder: (_, __) => MaterialApp(
+      builder: (context, __) => MaterialApp(
         title: flavor == AppFlavor.PROD
             ? 'Pay Flow'
             : flavor == AppFlavor.PREVIEW
@@ -37,6 +42,7 @@ class AppWidget extends StatelessWidget {
         darkTheme: AppTheme.darkTheme,
         debugShowCheckedModeBanner: false,
         routes: routes,
+        navigatorObservers: [context.read<AnalyticsController>().getObserver()],
       ),
     );
   }
